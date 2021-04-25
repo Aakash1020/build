2a15e78707dc6039e2541a3410e633cae595376a
Aakash Rathod <68330841+Aakash1020@users.noreply.github.com>
2021-04-25 18:42:10 +0530

    Add files via upload

---
+++ precache-manifest.3677a8dd4bfb8dcefd5d71bf80af43ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c3583de56b377e868dff1f6d36b540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "539b23da65231e1f4b24",
+    "url": "/static/css/main.bd6787d4.chunk.css"
+  },
+  {
+    "revision": "425e3effd03406519724",
+    "url": "/static/js/2.416fa8cc.chunk.js"
+  },
+  {
+    "revision": "7696b0c24cae6f554b255f338b9f3bde",
+    "url": "/static/js/2.416fa8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539b23da65231e1f4b24",
+    "url": "/static/js/main.c5cdcae7.chunk.js"
+  },
+  {
+    "revision": "c04465a6f8153566678c",
+    "url": "/static/js/runtime-main.f1d84136.js"
+  }
+]);
